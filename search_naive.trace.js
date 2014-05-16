ac489ba71e201a5edb78a8022c7720df17898548
Lucas Stadler <lu@papill0n.org>
2014-05-16 17:06:42 +0200

    fix traced naive search.
    (fixed line numbers and enabled tracing the initial writes to variables.

--- search_naive.trace.js
+++ search_naive.trace.js
@@ -1,15 +1,15 @@
-traceLine(1); var pattern = "Wor";
-traceLine(2); var string  = "Hello, World!";
+traceLine(1); var pattern = traceWrite("pattern", "Wor", true);
+traceLine(2); var string  = traceWrite("string", "Hello, World!", true);
 
-traceLine(4); for (var i = 0; traceLine(4),traceValue("i < string.length", i < string.length); traceLine(4),traceWrite("i", i++)) {
-	traceLine(5); var j = 0;
-	traceLine(5); while (traceLine(5),traceValue("j < pattern.length", j < pattern.length)
+traceLine(4); for (var i = traceWrite("i", 0, true); traceLine(4),traceValue("i < string.length", i < string.length); traceLine(4),traceWrite("i", i++)) {
+	traceLine(5); var j = traceWrite("j", 0, true);
+	traceLine(6); while (traceLine(6),traceValue("j < pattern.length", j < pattern.length)
 			&& traceValue("pattern[j] == string[i + j]",
 					traceRead("pattern[j]", pattern[j]) == traceRead("string[i + j]", string[i + j]))) {
-		traceLine(6); traceWrite("j", j++);
+		traceLine(7); traceWrite("j", j++);
 	}
 
-	traceLine(8); if (traceValue("j == pattern.length", traceRead("j", j) == traceRead("pattern.length", pattern.length))) {
-		traceLine(9); console.log("found match at " + traceRead("j", j));
+	traceLine(10); if (traceValue("j == pattern.length", traceRead("j", j) == traceRead("pattern.length", pattern.length))) {
+		traceLine(11); console.log("found match at " + traceRead("j", j));
 	}
 }
