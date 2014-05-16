9247844c7d0dbcbfde94bbfed78a94908daeac90
Lucas Stadler <lu@papill0n.org>
2014-05-16 16:16:49 +0200

    trace loop variables as well.

--- search_naive.trace.js
+++ search_naive.trace.js
@@ -1,9 +1,9 @@
 traceLine(1); var pattern = "Wor";
 traceLine(2); var string  = "Hello, World!";
 
-traceLine(4); for (var i = 0; traceLine(4),i < string.length; traceLine(4),traceWrite("i", i++)) {
+traceLine(4); for (var i = 0; traceLine(4),traceValue("i < string.length", i < string.length); traceLine(4),traceWrite("i", i++)) {
 	traceLine(5); var j = 0;
-	traceLine(5); while (traceLine(5),j < pattern.length
+	traceLine(5); while (traceLine(5),traceValue("j < pattern.length", j < pattern.length)
 			&& traceValue("pattern[j] == string[i + j]",
 					traceRead("pattern[j]", pattern[j]) == traceRead("string[i + j]", string[i + j]))) {
 		traceLine(6); traceWrite("j", j++);
